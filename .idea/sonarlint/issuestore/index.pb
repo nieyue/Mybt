6b57e0194578f3b03bb20179529b1f7ee8d349b6
nieyue <278076304@qq.com>
2020-05-06 19:21:48 +0800

    2020 5 6

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Y
+)src/main/java/com/nieyue/Application.java,8\d\8db134cdf49648d6d0a5eefb84bfc96c4b6fdee9
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/com/nieyue/Application.java
@@ -0,0 +1,69 @@
+package com.nieyue;
+
+import bt.Bt;
+import bt.TorrentClientBuilder;
+import bt.data.Storage;
+import bt.data.file.FileSystemStorage;
+import bt.dht.DHTConfig;
+import bt.dht.DHTModule;
+import bt.metainfo.Torrent;
+import bt.metainfo.TorrentSource;
+import bt.runtime.BtClient;
+import bt.runtime.BtRuntime;
+import bt.runtime.Config;
+import com.google.inject.Module;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
+
+public class Application {
+    //私有运行时的客户端
+    public static void self(){
+        // enable multithreaded verification of torrent data
+        Config config = new Config() {
+            @Override
+            public int getNumOfHashingThreads() {
+                return Runtime.getRuntime().availableProcessors() * 2;
+            }
+        };
+
+        config.setAcceptorPort(6899);
+        // enable bootstrapping from public routers
+        Module dhtModule = new DHTModule(new DHTConfig() {
+            @Override
+            public boolean shouldUseRouterBootstrap() {
+                return true;
+            }
+        });
+
+        //Path targetDirectory = Paths.get(System.getProperty("user.home"), "Downloads");
+        Path targetDirectory = Paths.get("E:\\nieyue\\ide\\bt\\bt-cli\\t");
+        Storage storage = new FileSystemStorage(targetDirectory);
+        URL url = null;
+        try {
+             //url=new URL("https://tbm-auth.alicdn.com/e99361edd833010b/IVDv9na5AeqibRSfuOP/gWmhp0e8gzFNFFM5q8J_251309133700_hd_hq.mp4?auth_key=1588749222-0-0-3d44ba0ff40b71d3f919f2c7e2efd3c0");
+            // url=new URL("file:///E:\\nieyue\\ide\\bt\\bt-cli\\a.torrent");
+            url=new File("E:\\nieyue\\ide\\bt\\bt-cli\\a.torrent").toURI().toURL();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        BtClient client = Bt.client()
+                .storage(storage)
+               .torrent(url)
+               // .magnet("magnet:?xt=urn:btih:A7CDEDE5F3468A93BA39B06BA209CD997CDB29BF&dn=SQTE-021&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce")
+                //.magnet("magnet:?xt=urn:btih:AF0D9AA01A9AE123A73802CFA58CCAF355EB19F0")
+                .autoLoadModules()
+                .module(dhtModule)
+                .stopWhenDownloaded()
+                .build();
+
+        client.startAsync().join();
+    }
+    public static void main(String[] args) {
+        self();
+    }
+}
